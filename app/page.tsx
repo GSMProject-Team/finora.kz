5cc3667b2a3a528939bd1914480cd9841438e093
GSMProject-Team <gsm.1095.z@gmail.com>
2026-02-08 21:36:11 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Finora</h1>
+      <p>Finora — бухгалтерлік, қаржылық және заңгерлік қызметтер платформасы.</p>
+    </main>
+  );
+}
+
+feat: add home page
